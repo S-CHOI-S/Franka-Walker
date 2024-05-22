a132fe7dbabdd6148c0194cd81aeb0592c2a571b
S-CHOI-S <jennychoi0904@gmail.com>
2024-05-22 18:58:47 +0900

    240522

--- src/ViT.py
+++ src/ViT.py
@@ -1,299 +1,117 @@
+import cv2
 import torch
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
 
-from torch import nn, optim
-from torch import Tensor
+import numpy as np
+
 from PIL import Image
-from torchvision.transforms import Compose, Resize, ToTensor
-from einops import rearrange, reduce, repeat
-from einops.layers.torch import Rearrange, Reduce
-from torchsummary import summary
+from torchvision import transforms
 
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+model = torch.hub.load('facebookresearch/deit:main', 'deit_tiny_patch16_224', pretrained=True)
 
-# batch_size = 8
-# channel = 3
-# h, w = (224, 224)
-x = torch.randn(8, 3, 224, 224)
-plt.imshow(x[0].permute(1,2,0).numpy()) # (C, H, W) to (H, W, C)
-plt.show()
-print('x:', x.shape) # x: torch.Size([8, 3, 224, 224])
-
-## for patch embedding
-'''
-    [batch] x [channel] x [height] x [width]의 구조를 가진 image를 
-    다음과 같은 vector로 embedding해주어야 함
-
-    -> [batch] x [n] x ([p] x [p] x [channel])
-        - [p]: patch size
-        - [n]: number of patch size = [height] x [width] / ([p] x [p])
-'''
-
-'''
-## einops를 사용해서 image를 patch로 나누고 flatten하는 과정
-patch_size = 16 # 16 pixels
-patches = rearrange(x, 'b c (h s1) (w s2) -> b (h w) (s1 s2 c)', s1 = patch_size, s2 = patch_size)
-print('patches:', patches.shape) # patches: torch.Size([8, 196, 768])
-
-
-## ViT: kernel size와 stride size를 patch size로 갖는 Conv2D를 이용해서 flatten
-patch_size = 16
-in_channels = 3
-emb_size = 768
-img_size = 224
-
-projection = nn.Sequential(
-    # using a conv layer instead of a linear one -> performance gains
-    nn.Conv2d(in_channels, emb_size, kernel_size = patch_size, stride = patch_size),
-    Rearrange('b e (h) (w) -> b (h w) e'),
-)
-
-# image를 patch로 나누고, flatten
-projected_x = projection(x)
-print('projected_x:', projected_x.shape) # projected_x: torch.Size([8, 196, 768])
-
-# cls_token과 pos encoding parameter 정의
-cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
-positions = nn.Parameter(torch.randn((img_size // patch_size)**2 + 1, emb_size))
-print('cls shape:', cls_token.shape) # cls shape: torch.Size([1, 1, 768])
-print('pos shape:', positions.shape) # pos shape: torch.Size([197, 768])
-
-# cls_token을 반복하여 batch_size의 크기와 맞춰줌
-batch_size = 8
-cls_tokens = repeat(cls_token, '() n e -> b n e', b = batch_size)
-print('repeated cls shape:', cls_tokens.shape) # repeated cls shape: torch.Size([8, 1, 768])
-
-# cls_token과 projected_x를 concatenate (배열 합치기)
-cat_x = torch.cat([cls_tokens, projected_x], dim = 1)
-
-# position encoding을 더해줌
-cat_x += positions
-print('output:', cat_x.shape) # output: torch.Size([8, 197, 768])
-'''
-
-class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
-        self.patch_size = patch_size
-        super().__init__()
-        self.projection = nn.Sequential(
-            # using a conv layer instead of a linear one -> performance gains
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
-            Rearrange('b e (h) (w) -> b (h w) e'),
-        )
-        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
-        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
-        
-    def forward(self, x: Tensor) -> Tensor:
-        b, _, _, _ = x.shape
-        x = self.projection(x)
-        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b = b)
-        # prepend the cls token to the input
-        x = torch.cat([cls_tokens, x], dim=1)
-        # add position embedding
-        x += self.positions
-
-        return x
-    
-# x = PatchEmbedding()(x)
-
-## for multi head attention
-'''
-# linear projection (q, k, v)
-emb_size = 768
-num_heads = 8
-
-keys = nn.Linear(emb_size, emb_size)
-queries = nn.Linear(emb_size, emb_size)
-values = nn.Linear(emb_size, emb_size)
-print('keys:', keys) # keys: Linear(in_features=768, out_features=768, bias=True)
-print('queries:', queries) # queries: Linear(in_features=768, out_features=768, bias=True)
-print('values:', values) # values: Linear(in_features=768, out_features=768, bias=True)
-
-# multi-head
-queries = rearrange(queries(x), "b n (h d) -> b h n d", h = num_heads)
-keys = rearrange(keys(x), "b n (h d) -> b h n d", h = num_heads)
-values = rearrange(values(x), "b n (h d) -> b h n d", h = num_heads)
-print('queries shape:', queries.shape) # queries shape: torch.Size([8, 8, 197, 96])
-print('keys shape:', keys.shape) # keys shape: torch.Size([8, 8, 197, 96])
-print('values shape:', values.shape) # values shape: torch.Size([8, 8, 197, 96])
-
-# queries x keys
-energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)
-print('energy:', energy.shape) # energy: torch.Size([8, 8, 197, 197])
-
-# get attention score
-scaling = emb_size ** (1/2)
-att = F.softmax(energy, dim=-1) / scaling
-print('att:', att.shape) # att: torch.Size([8, 8, 197, 197])
-
-# attention score x values
-out = torch.einsum('bhal, bhlv -> bhav', att, values)
-print('out:', out.shape) # out: torch.Size([8, 8, 197, 96])
-
-# rearrange to emb_size
-out = rearrange(out, 'b h n d -> b n (h d)')
-print('out2:', out.shape) # out2: torch.Size([8, 197, 768])
-'''
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
-        super().__init__()
-        self.emb_size = emb_size
-        self.num_heads = num_heads
-        # fuse the queries, keys and values in one matrix
-        self.qkv = nn.Linear(emb_size, emb_size * 3)
-        self.att_drop = nn.Dropout(dropout)
-        self.projection = nn.Linear(emb_size, emb_size)
-        
-    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
-        # split keys, queries and values in num_heads
-        qkv = rearrange(self.qkv(x), "b n (h d qkv) -> (qkv) b h n d", h=self.num_heads, qkv=3)
-        queries, keys, values = qkv[0], qkv[1], qkv[2]
-        # sum up over the last axis
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
-        if mask is not None:
-            fill_value = torch.finfo(torch.float32).min
-            energy.mask_fill(~mask, fill_value)
-            
-        scaling = self.emb_size ** (1/2)
-        att = F.softmax(energy, dim=-1) / scaling
-        att = self.att_drop(att)
-        # sum up over the third axis
-        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        out = self.projection(out)
-        return out
-    
-class ResidualAdd(nn.Module):
-    def __init__(self, fn):
-        super().__init__()
-        self.fn = fn
-        
-    def forward(self, x, **kwargs):
-        res = x
-        x = self.fn(x, **kwargs)
-        x += res
-        return x
-    
-class FeedForwardBlock(nn.Sequential):
-    def __init__(self, emb_size: int, expansion: int = 4, drop_p: float = 0.):
-        super().__init__(
-            nn.Linear(emb_size, expansion * emb_size),
-            nn.GELU(),
-            nn.Dropout(drop_p),
-            nn.Linear(expansion * emb_size, emb_size),
-        )
-
-class TransformerEncoderBlock(nn.Sequential):
-    def __init__(self,
-                 emb_size: int = 768,
-                 drop_p: float = 0.,
-                 forward_expansion: int = 4,
-                 forward_drop_p: float = 0.,
-                 ** kwargs):
-        super().__init__(
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                MultiHeadAttention(emb_size, **kwargs),
-                nn.Dropout(drop_p)
-            )),
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                FeedForwardBlock(
-                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
-                nn.Dropout(drop_p)
-            )
-            ))
-        
-class TransformerEncoder(nn.Sequential):
-    def __init__(self, depth: int = 12, **kwargs):
-        # stack encoder block amount of depth
-        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
-
-class ClassificationHead(nn.Sequential):
-    def __init__(self, emb_size: int = 768, n_classes: int = 1000):
-        super().__init__(
-            Reduce('b n e -> b e', reduction='mean'),
-            nn.LayerNorm(emb_size), 
-            nn.Linear(emb_size, n_classes))
-        
-class ViT(nn.Sequential):
-    def __init__(self,     
-                in_channels: int = 3,
-                patch_size: int = 16,
-                emb_size: int = 768,
-                img_size: int = 224,
-                depth: int = 12,
-                n_classes: int = 1000,
-                **kwargs):
-        super().__init__(
-            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
-            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
-            ClassificationHead(emb_size, n_classes)
-        )
-                
-patches_embedded = PatchEmbedding()(x)
-print('Patches embedded shape:', patches_embedded.shape)
-print('MultiHeadAttention output shape:', MultiHeadAttention()(patches_embedded).shape)
-print('TransformerEncoderBlock output shape:', TransformerEncoderBlock()(patches_embedded).shape)
-
-model = ViT()
-print(model)
-summary(model, (3, 224, 224), device='cpu')
-
-# Prepare the MNIST dataset
 transform = transforms.Compose([
-    transforms.Resize((28, 28)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 ])
 
-train_dataset = datasets.MNIST(root='data', train=True, transform=transform, download=True)
-test_dataset = datasets.MNIST(root='data', train=False, transform=transform, download=True)
+img = Image.open('/home/kist/franka_panda_3_mppi/src/vit_explain/examples/both.png')
+img.resize((224, 224))
+input_tensor = transform(img).unsqueeze(0)
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+if torch.cuda.is_available():
+    input_tensor = input_tensor.cuda()
+    model = model.cuda()
+    
+print("Successed!")
 
-# Instantiate the model, loss function and optimizer
-model = ViT()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Training function
-def train(model, loader, criterion, optimizer, epoch):
-    model.train()
-    running_loss = 0.0
-    for i, (inputs, labels) in enumerate(loader):
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        if i % 100 == 99:
-            print(f'Epoch {epoch+1}, Batch {i+1}, Loss: {running_loss / 100}')
-            running_loss = 0.0
-
-# Testing function
-def test(model, loader, criterion):
-    model.eval()
-    correct = 0
-    total = 0
-    test_loss = 0.0
+def rollout(attentions, discatd_ratio, head_fusion):
+    '''
+    attentions: [1, 3, 197, 197] size의 tensor가 12개 담긴 list
+    '''
+    result = torch.eye(attentions[0].size(-1)).cuda()
+    
     with torch.no_grad():
-        for inputs, labels in loader:
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            test_loss += loss.item()
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = 100 * correct / total
-    print(f'Test Loss: {test_loss / len(loader)}, Accuracy: {accuracy}%')
+        for attention in attentions:
+            if head_fusion == 'mean':
+                attention_heads_fused = attention.mean(axis=1)
+            elif head_fusion == 'max':
+                attention_heads_fused = attention.max(axis=1)[0]
+            elif head_fusion == 'min':
+                attention_heads_fused = attention.min(axis=1)[0]
+            else:
+                raise 'Attention head fusion type Not supported'
+            
+            # Drop the lowest attentions, but don't drop the class token
+            flat = attention_heads_fused.view(attention_heads_fused.size(0), -1)
+            _, indices = flat.topk(int(flat.size(-1)*discatd_ratio), dim = -1, largest = False)
+            indices = indices[indices != 0]
+            flat[0, indices] = 0
+            
+            I = torch.eye(attention_heads_fused.size(-1)).cuda()
+            a = (attention_heads_fused + 1.0 * I) / 2
+            a = a / a.sum(dim = -1)
+            
+            result = torch.matmul(a, result) # [1, 197, 197]
+            
+    # look at the total attention between the class token,
+    # and the image patches
+    mask = result[0,0,1:] # [196, 196]
+    
+    # in case of 224 x 224 image, this brings us from 196 to 14
+    width = int(mask.size(-1)**0.5)
+    mask = mask.reshape(width, width).cpu().numpy()
+    mask = mask / np.max(mask)
+    return mask # [14, 14]
 
-# Training and testing the model
-for epoch in range(5):
-    train(model, train_loader, criterion, optimizer, epoch)
-    test(model, test_loader, criterion)
+class ViTAttentionRollout:
+    def __init__(self, model, attention_layer_name='attn_drop', head_fusion='mean', discard_ratio=0.9):
+        self.model = model
+        self.head_fusion = head_fusion
+        self.discard_ratio = discard_ratio
+        
+        for name, module in self.model.named_modules():
+            print(name)
+            if attention_layer_name in name:
+                module.register_forward_hook(self.get_attention)
+                print(f"Hook registered for {name}")
+                
+        self.attentions = []
+        
+    def get_attention(self, module, input, output):
+        print(f"Hook called for {module}")
+        self.attentions.append(output.cpu())
+        
+    def __call__(self, input_tensor):
+        self.attentions = []
+        
+        with torch.no_grad():
+            output = self.model(input_tensor)
+            
+        if not self.attentions:
+            raise RuntimeError("No attentions were collected. Check the attention layer name and forward hook.")
+        
+        return rollout(self.attentions, self.discard_ratio, self.head_fusion)
+    
+def show_mask_on_image(img, mask):
+    img = np.float32(img) / 255
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+
+
+model = torch.hub.load('facebookresearch/deit:main', 'deit_tiny_patch16_224', pretrained=True)
+if torch.cuda.is_available():
+    model = model.cuda()
+grad_rollout = ViTAttentionRollout(model, discard_ratio=0.9, head_fusion='max')
+mask = grad_rollout(input_tensor)
+
+np_img = np.array(img)[:, :, ::-1]
+mask = cv2.resize(mask, (np_img.shape[1], np_img.shape[0]))
+mask = show_mask_on_image(np_img, mask)
+
+cv2.imshow("Attention Rollout", mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from .mjc_pybind import *
+from .vit_explain import *
